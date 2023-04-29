e15926922d387763b56b647aa4a26a52846bc3ff
FarehaSi <>
2023-04-29 07:42:23 +0000

    Build the addTurn function

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
- const { game, newGame, showScore } = require("../game");
+ const { game, newGame, showScore, addTurn } = require("../game");
 
 
  beforeAll(() => {
@@ -45,8 +45,8 @@
     test("should clear the player moves array", () => {
         expect(game.playerMoves.length).toBe(0);
     });
-    test("should clear the computer sequence array", () => {
-        expect(game.currentGame.length).toBe(0);
+     test("should add one move to the computer's game array", () => {
+        expect(game.currentGame.length).toBe(1);
     });
      test("should display 0 for the element with id of score", () => {
         expect(document.getElementById("score").innerText).toEqual(0);
